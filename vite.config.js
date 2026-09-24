31f1aaedbc102b27876ebe343b8d3678aaa07732
Hassan Mahmoud <officialhassansmail@gmail.com>
2026-09-24 17:04:18 +0300

    change base link, again

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "https://github.com/hshs-dev/memory-game",
+  base: "/memory-game/",
   plugins: [react(), tailwindcss()],
 });
